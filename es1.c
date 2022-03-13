36754448f3409e5593810b374904620505b0bb03
EmmaMilani <94352149+EmmaMilani@users.noreply.github.com>
2022-03-13 18:17:09 +0100

    es1 cartella array

--- es1.c
+++ es1.c
@@ -1,26 +1,17 @@
 #include <stdio.h>
-int main(int argc, char*argv[])
+#define DIM 4
+int main (int argc; char*argv[])
 {
-    int num1, num2;
-    double rapp;
-
-    do
+    int numeri[DIM];
+    int n;
+    int i;
+    for(i=0; i < DIM; i++)
     {
-        printf("Inserisci il primo numero:\n");
-        scanf("%d", & num1); //uso %d perché num1 è un int
-        printf("Inserisci il secondo numero:\n");
-        scanf("%d", & num2);
-        if(num1 < num2)
-        {
-            rapp = (double)num2 / (double)num1;
-        }
-        else
-        {
-            rapp = (double)num1 / (double)num2;
-        }
-        printf("Il risultato è: %f", rapp); //utilizzo %f perchè rapp è in double
-
-    }while(a!= 0 && b!=0)
-    
-    return 0;
+        printf("Inserisci numero:\n");
+        scanf("%d", & numeri[i]);
+    }
+    printf("L'array ha questi numeri:\n");
+    for(i=0; i < DIM; i++){
+        printf("%d\n", numeri[i]);
+    }
 }
